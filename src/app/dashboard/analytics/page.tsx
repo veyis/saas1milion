76e11925c4a3e7a178ec506414541dd2f797f5e1
veyis <vakgul@gmail.com>
2024-10-24 10:26:25 -0700

    v4

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,82 +1,221 @@
-import { subscriptionTiers } from "@/data/subscriptionTiers";
-import { db } from "@/drizzle/db";
-import { UserSubscriptionTable } from "@/drizzle/schema";
-import { CACHE_TAGS, dbCache, getUserTag, revalidateDbCache } from "@/lib/cache";
-import { SQL } from "drizzle-orm";
+import { HasPermission } from "@/components/HasPermission"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import {
+  CHART_INTERVALS,
+  getViewsByCountryChartData,
+  getViewsByDayChartData,
+  getViewsByPPPChartData,
+} from "@/server/db/productViews"
+import { canAccessAnalytics } from "@/server/permissions"
+import { auth } from "@clerk/nextjs/server"
+import { ChevronDownIcon, SearchCheck } from "lucide-react"
+import { ViewsByCountryChart } from "../_components/charts/ViewsByCountryChart"
+import { ViewsByPPPChart } from "../_components/charts/ViewsByPPPChart"
+import { ViewsByDayChart } from "../_components/charts/ViewsByDayChart"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { createURL } from "@/lib/utils"
+import { getProduct, getProducts } from "@/server/db/products"
+import { TimezoneDropdownMenuItem } from "../_components/TimezoneDropdownMenuItem"
 
-export async function createUserSubscription(
-  data: typeof UserSubscriptionTable.$inferInsert
+export default async function AnalyticsPage({
+  searchParams,
+}: {
+  searchParams: {
+    interval?: string
+    timezone?: string
+    productId?: string
+  }
+}) {
+  const { userId, redirectToSignIn } = auth()
+  if (userId == null) return redirectToSignIn()
+
+  const interval =
+    CHART_INTERVALS[searchParams.interval as keyof typeof CHART_INTERVALS] ??
+    CHART_INTERVALS.last7Days
+  const timezone = searchParams.timezone || "UTC"
+  const productId = searchParams.productId
+
+  return (
+    <>
+      <div className="mb-6 flex justify-between items-baseline">
+        <h1 className="text-3xl font-semibold">Analytics</h1>
+        <HasPermission permission={canAccessAnalytics}>
+          <div className="flex gap-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  {interval.label}
+                  <ChevronDownIcon className="size-4 ml-2" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {Object.entries(CHART_INTERVALS).map(([key, value]) => (
+                  <DropdownMenuItem asChild key={key}>
+                    <Link
+                      href={createURL("/dashboard/analytics", searchParams, {
+                        interval: key,
+                      })}
+                    >
+                      {value.label}
+                    </Link>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <ProductDropdown
+              userId={userId}
+              selectedProductId={productId}
+              searchParams={searchParams}
+            />
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  {timezone}
+                  <ChevronDownIcon className="size-4 ml-2" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem asChild>
+                  <Link
+                    href={createURL("/dashboard/analytics", searchParams, {
+                      timezone: "UTC",
+                    })}
+                  >
+                    UTC
+                  </Link>
+                </DropdownMenuItem>
+                <TimezoneDropdownMenuItem searchParams={searchParams} />
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </HasPermission>
+      </div>
+      <HasPermission permission={canAccessAnalytics} renderFallback>
+        <div className="flex flex-col gap-8">
+          <ViewsByDayCard
+            interval={interval}
+            timezone={timezone}
+            userId={userId}
+            productId={productId}
+          />
+          <ViewsByPPPCard
+            interval={interval}
+            timezone={timezone}
+            userId={userId}
+            productId={productId}
+          />
+          <ViewsByCountryCard
+            interval={interval}
+            timezone={timezone}
+            userId={userId}
+            productId={productId}
+          />
+        </div>
+      </HasPermission>
+    </>
+  )
+}
+
+async function ProductDropdown({
+  userId,
+  selectedProductId,
+  searchParams,
+}: {
+  userId: string
+  selectedProductId?: string
+  searchParams: Record<string, string>
+}) {
+  const products = await getProducts(userId)
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          {products.find(p => p.id === selectedProductId)?.name ??
+            "All Products"}
+          <ChevronDownIcon className="size-4 ml-2" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem asChild>
+          <Link
+            href={createURL("/dashboard/analytics", searchParams, {
+              productId: undefined,
+            })}
+          >
+            All Products
+          </Link>
+        </DropdownMenuItem>
+        {products.map(product => (
+          <DropdownMenuItem asChild key={product.id}>
+            <Link
+              href={createURL("/dashboard/analytics", searchParams, {
+                productId: product.id,
+              })}
+            >
+              {product.name}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+async function ViewsByDayCard(
+  props: Parameters<typeof getViewsByDayChartData>[0]
 ) {
-  const [newSubscription] = await db
-    .insert(UserSubscriptionTable)
-    .values(data)
-    .onConflictDoNothing({
-      target: UserSubscriptionTable.clerkUserId,
-    })
-    .returning({
-      id: UserSubscriptionTable.id,
-      userId: UserSubscriptionTable.clerkUserId,
-    });
+  const chartData = await getViewsByDayChartData(props)
 
-  if (newSubscription != null) {
-    await revalidateDbCache({
-      tag: CACHE_TAGS.subscription,
-      id: newSubscription.id,
-      userId: newSubscription.userId,
-    });
-  }
-
-  return newSubscription;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Visitors Per Day</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ViewsByDayChart chartData={chartData} />
+      </CardContent>
+    </Card>
+  )
 }
 
-export async function getUserSubscription(userId: string) {
-  const cacheFn = dbCache(getUserSubscriptionInternal, {
-    tags: [getUserTag(userId, CACHE_TAGS.subscription)],
-  });
-
-  return await cacheFn(userId);
-}
-
-export async function updateUserSubscription(
-  where: SQL,
-  data: Partial<typeof UserSubscriptionTable.$inferInsert>
+async function ViewsByPPPCard(
+  props: Parameters<typeof getViewsByPPPChartData>[0]
 ) {
-  const [updatedSubscription] = await db
-    .update(UserSubscriptionTable)
-    .set(data)
-    .where(where)
-    .returning({
-      id: UserSubscriptionTable.id,
-      userId: UserSubscriptionTable.clerkUserId,
-    });
+  const chartData = await getViewsByPPPChartData(props)
 
-  if (updatedSubscription != null) {
-    await revalidateDbCache({
-      tag: CACHE_TAGS.subscription,
-      userId: updatedSubscription.userId,
-      id: updatedSubscription.id,
-    });
-  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Visitors Per PPP Group</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ViewsByPPPChart chartData={chartData} />
+      </CardContent>
+    </Card>
+  )
 }
 
-export async function getUserSubscriptionTier(userId: string) {
-  const subscription = await getUserSubscription(userId);
+async function ViewsByCountryCard(
+  props: Parameters<typeof getViewsByCountryChartData>[0]
+) {
+  const chartData = await getViewsByCountryChartData(props)
 
-  if (subscription == null) {
-    console.warn(`No subscription found for user ID: ${userId}. Assigning default "Free" tier.`);
-    return subscriptionTiers["Free"];
-  }
-
-  if (!subscription.tier || !(subscription.tier in subscriptionTiers)) {
-    console.warn(`Invalid subscription tier for user ID: ${userId}. Assigning default "Free" tier.`);
-    return subscriptionTiers["Free"];
-  }
-
-  return subscriptionTiers[subscription.tier as keyof typeof subscriptionTiers];
-}
-
-async function getUserSubscriptionInternal(userId: string) {
-  return await db.query.UserSubscriptionTable.findFirst({
-    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
-  });
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Visitors Per Country</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ViewsByCountryChart chartData={chartData} />
+      </CardContent>
+    </Card>
+  )
 }
